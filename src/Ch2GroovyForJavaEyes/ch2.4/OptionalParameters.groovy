7849f7730d17069e6bc3af2d8534314aff80de19
yupenglei <yupengniaosanshi@gmail.com>
2019-02-03 12:49:25 +0800

    2.4 可选形参

---
+++ src/Ch2GroovyForJavaEyes/ch2.4/OptionalParameters.groovy
@@ -0,0 +1,22 @@
+/**
+ * 可选参
+ */
+def log(x, base = 10) {
+    Math.log(x) / Math.log(base)
+}
+
+println log(1024)
+println log(1024, 10)
+println log(1024, 2)
+
+/**
+ * 末尾的数组形参视为可选参
+ * Groovy会把末尾的实参收集起来赋给数组形参
+ */
+def task(name, String[] details) {
+    println "$name - $details"
+}
+
+task 'Call', '123-456-7890'
+task 'Call', '123-456-7890', '231-546-0987'
+task 'Check Mail'
