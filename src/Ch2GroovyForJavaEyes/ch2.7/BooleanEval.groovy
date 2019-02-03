717fd56cddd3885f66cb78f1fcf467245da320b1
yupenglei <yupengniaosanshi@gmail.com>
2019-02-03 12:50:38 +0800

    2.7 布尔求值

---
+++ src/Ch2GroovyForJavaEyes/ch2.7/BooleanEval.groovy
@@ -0,0 +1,26 @@
+/**
+ * 布尔求值
+ */
+str = 'hello'
+if (str) {//非null非空则为true
+    println 'hello world'
+}
+
+// 集合非null，非空则为true
+lst0 = null
+println lst0 ? 'lst0 true' : 'lst0 false'
+lst1 = [1, 2, 3]
+println lst1 ? 'lst1 true' : 'lst1 false'
+lst2 = []
+println lst2 ? 'lst2 true' : 'lst2 false'
+
+// 对象可通过实现asBoolean方法来决定自己的布尔转换
+class Hello {
+    def asBoolean() {
+        false
+    }
+}
+
+if (new Hello()){
+    println "Hello() is not null"
+}
